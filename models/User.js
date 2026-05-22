a173802e98cf0207105531b65ca6881d3b631440
zaherinfinity <vavannak364@gmail.com>
2026-05-22 20:45:16 +0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  // ... same fields as earlier (email, password, name, role)
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.models.User || mongoose.model('User', userSchema);
